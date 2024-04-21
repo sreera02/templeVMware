58be2e1640523605ea6577f8a7ebf6d0e0806cfc
sreera02 <srr_developer@protonmail.com>
2024-04-21 13:46:11 -0400

    fixed bug

--- src/index.js
+++ src/index.js
@@ -54,8 +54,5 @@ app.on('window-all-closed', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-const enterbutton = document.querySelector('.form-boxlogin');
-enterbutton.addEventListener('click', ()=> {
-  form-boxlogin.classlist.add('enterbutton');
-});
+
 
